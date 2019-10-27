47d8ba9c4b85241b880692f5e704e5c96fde3dab
Hussain Ahmed <haz6798@icloud.com>
2019-10-27 03:28:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,17 +23,17 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  V[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  V[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  V[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+V[2]="4"
 end
